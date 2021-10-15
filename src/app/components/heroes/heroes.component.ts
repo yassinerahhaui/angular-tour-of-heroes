455f06018595160741e57e8594c8de93fe5d0159
yassinerahhaui <yassinerahhaoui12@gmail.com>
2021-10-15 07:51:31 +0100

    add basics of this project

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Hero } from './../../hero';
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../../moch-hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero?: Hero;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
---
+++ src/app/moch-hero.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {'id': 1, 'name': 'yassine'},
+  {'id': 2, 'name': 'ayoub'},
+  {'id': 3, 'name': 'youssef'},
+  {'id': 4, 'name': 'zakaria'},
+  {'id': 5, 'name': 'amine'},
+  {'id': 6, 'name': 'hamza'},
+  {'id': 7, 'name': 'mohamed'},
+  {'id': 8, 'name': 'nisrine'},
+  {'id': 9, 'name': 'sabrine'},
+  {'id': 10, 'name': 'aya'},
+  {'id': 11, 'name': 'kawtar'},
+]
